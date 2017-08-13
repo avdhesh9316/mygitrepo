05804d5a219832ef67b5d7b3fbb5b4212315ac43
avdhesh9316 <divyam@gmail.com>
2017-08-13 04:45:29 +0000

    sovle the errors

--- welcome.py
+++ welcome.py
@@ -3,7 +3,8 @@
 print "I am welcome.py file"
 
 num = raw_input("Enter any number: ")
-for i in range(0, num):
+for i in range(0, int(num)):
 	print "Welcome Sir"
 
 
+print "It is the end of welcome file"
